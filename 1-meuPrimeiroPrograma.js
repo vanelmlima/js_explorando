36f04cae9c296747343bef24a6ee3e2bb2ed4123
vanelmlima <vlml2001lima@gmail.com>
2022-12-02 15:15:28 -0200

    variaveis

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,13 @@
 console.log("Meu Primeiro Programa");
+
+const idade = 21;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+idadeSomada = idade + 2;
+
+console.log(idadeSomada);
